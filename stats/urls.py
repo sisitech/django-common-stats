cee82d11874e5602892fdec733b0630c523b12bd
Mwangi Micha <michameiu@gmail.com>
2023-06-01 11:41:51 +0300

    Add test templates

--- stats/urls.py
+++ stats/urls.py
@@ -1,12 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from stats.views import OptimizedExportTriggerAPIView
 
 
 urlpatterns = [
-    url(r'^$', OptimizedExportTriggerAPIView.as_view(), name="trigger_export"),
-# url(r'^attendances/(?P<type>.+)/?$', ListAttendanceDynamicStatisticsAPIView.as_view(),
-#         name="list_dynamic_attendances_statistics"),
-#     url(r'^students/(?P<type>.+)/?$', ListStudentsDynamicsAPIView.as_view(),
-#         name="list_dynamic_students_statistics"),
+    url(r"^$", OptimizedExportTriggerAPIView.as_view(), name="trigger_export"),
+    url(r"^test-reports/", include("reports.urls")),
+    # url(r'^attendances/(?P<type>.+)/?$', ListAttendanceDynamicStatisticsAPIView.as_view(),
+    #         name="list_dynamic_attendances_statistics"),
+    #     url(r'^students/(?P<type>.+)/?$', ListStudentsDynamicsAPIView.as_view(),
+    #         name="list_dynamic_students_statistics"),
 ]
